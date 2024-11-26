e9394d33d3386dedb104084e1048349d1e18598e
reoreo-zyt <768119359@qq.com>
2024-11-27 05:49:17 +0800

    feat: 面试题目

--- projects/vue2-vite/src/main.js
+++ projects/vue2-vite/src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import { createPinia, PiniaVuePlugin } from 'pinia'
-
 import App from './App.vue'
+// TODO: 按需引入样式？
+import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 
 Vue.use(PiniaVuePlugin)
--- projects/vue2-vite/src/views/HomeView.vue
+++ projects/vue2-vite/src/views/HomeView.vue
@@ -1,3 +1,83 @@
+<template>
+	<div>
+		<div>
+			<el-row style="height: 95vh">
+				<el-col :span="6">
+					<el-menu :default-active="defaultActive" @select="selectMenu">
+						<el-submenu
+							index="1"
+							v-for="(item, index) in menus"
+							:key="index + 'submenu'"
+						>
+							<template slot="title">
+								<i :class="item.icon"></i>
+								<span>{{ item.title }}</span>
+							</template>
+							<el-menu-item
+								v-for="(citem, cindex) in item.children"
+								:key="cindex + 'submenu-item'"
+								:index="citem.index"
+								><i class="el-icon-tickets"></i>{{ citem.title }}</el-menu-item
+							>
+						</el-submenu>
+					</el-menu>
+				</el-col>
+				<el-col :span="18" style="height: 100%; overflow-y: scroll">
+					<el-row v-if="defaultActive == '1-1'" style="margin: 10px">
+						<el-card
+							style="margin-bottom: 10px"
+							v-for="(item, index) in welcomeItemData"
+							:key="index + 'box-card'"
+						>
+							<div slot="header" class="clearfix">
+								<span>{{ item.heading }}</span>
+								<el-button style="float: right; padding: 3px 0" type="text">
+									{{ item.version }}
+									<a :href="item.link" target="_blank"> 文档</a></el-button
+								>
+							</div>
+							<div>{{ item.description }}</div>
+						</el-card>
+					</el-row>
+					<el-row v-if="defaultActive == '2-1'" style="margin: 10px">
+						<div>默认方式显示金额</div>
+						<t-input
+							placeholder="请输入金额"
+							@numblur="blurVal"
+							v-model="money"
+						></t-input>
+						<div>显示金额tip（大写）控制isTip</div>
+						<t-input placeholder="请输入金额" isTip v-model="money2"></t-input>
+						<div>显示天数</div>
+						<t-input
+							placeholder="请输入天数"
+							inputType="days"
+							appendTitle="天"
+							@numblur="blurVal1"
+							v-model="day"
+						></t-input>
+						<div>显示百分比</div>
+						<t-input
+							placeholder="请输入"
+							inputType="percent"
+							appendTitle="%"
+							v-model="percent"
+						></t-input>
+						<div>显示电话号码</div>
+						<t-input
+							placeholder="请输入电话号码"
+							:isShow="false"
+							inputType="tel"
+							@numblur="blurVal2"
+							v-model="tel"
+						></t-input>
+					</el-row>
+				</el-col>
+			</el-row>
+		</div>
+	</div>
+</template>
+
 <script>
 export default {
 	name: 'HomeView',
@@ -37,52 +117,61 @@ export default {
 					description: '一个图标库，目前装载了mdi图标'
 				},
 				{
-					heading: 'unplugin-icons',
-					version: '^0.20.1',
-					link: 'https://icon-sets.iconify.design/mdi',
-					description: '一个图标库，目前装载了mdi图标'
+					heading: 'element-ui',
+					version: '^2.15.14',
+					link: 'https://element.eleme.cn/#/zh-CN',
+					description:
+						'Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库'
 				}
-			]
+			],
+			defaultActive: '1-1',
+			menus: [
+				{
+					index: '1',
+					icon: 'el-icon-folder',
+					title: '项目搭建',
+					children: [
+						{
+							index: '1-1',
+							icon: 'el-icon-tickets',
+							title: '文档'
+						}
+					]
+				},
+				{
+					index: '2',
+					icon: 'el-icon-folder',
+					title: 'element-ui 二次开发组件库',
+					children: [
+						{
+							index: '2-1',
+							icon: 'el-icon-tickets',
+							title: 'Input 组件'
+						}
+					]
+				}
+			],
+			// t-input
+			money: null,
+			day: null,
+			money2: null,
+			percent: null,
+			tel: ''
+		}
+	},
+	methods: {
+		selectMenu(index) {
+			this.defaultActive = index
+		},
+		blurVal(val) {
+			console.log('获取输入的值', val)
+		},
+		blurVal1(val) {
+			console.log('获取输入的值1', val)
+		},
+		blurVal2(val) {
+			console.log('获取输入的值2', val)
 		}
 	}
 }
 </script>
-
-<template>
-	<div>
-		<div>
-			<el-row style="height: 95vh">
-				<el-col :span="6">
-					<el-menu default-active="1-4-1">
-						<el-submenu index="1">
-							<template slot="title">
-								<i class="el-icon-folder"></i>
-								<span>项目搭建</span>
-							</template>
-							<el-menu-item index="1-4-1"
-								><i class="el-icon-tickets"></i>文档</el-menu-item
-							>
-						</el-submenu>
-					</el-menu>
-				</el-col>
-				<el-col :span="18" style="height: 100%; overflow-y: scroll">
-					<el-row>
-						<el-card
-							style="margin-bottom: 10px"
-							v-for="(item, index) in welcomeItemData"
-							:key="index + 'box-card'"
-						>
-							<div slot="header" class="clearfix">
-								<span>{{ item.heading }}</span>
-								<el-button style="float: right; padding: 3px 0" type="text"
-									><a :href="item.link" target="_blank">文档</a></el-button
-								>
-							</div>
-							<div>{{ item.description }}</div>
-						</el-card>
-					</el-row>
-				</el-col>
-			</el-row>
-		</div>
-	</div>
-</template>
